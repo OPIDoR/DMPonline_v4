837e636e35723a8c16813757384d798235eeba07
Benjamin FAURE <benjamin.faure@inist.fr>
2016-06-29 09:12:50 +0200

    Changed mailer url to localhost for development purpose

--- config/application.rb
+++ config/application.rb
@@ -72,7 +72,7 @@ module DMPonline4
     config.autoload_paths += %W(#{config.root}/lib)
 
     # Set the default host for mailer URLs
-    config.action_mailer.default_url_options = { :host => ENV["DMP_SERVER_NAME"] + ":" +ENV["APP_HTTP_PORT"] }
+    config.action_mailer.default_url_options = { :host => "localhost:3000" }
     config.active_record.whitelist_attributes = true
 
      # Enable shibboleth as an alternative authentication method
