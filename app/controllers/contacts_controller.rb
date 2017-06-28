81dfc8abccbac489e8e8f438912d6d145e076245
FAURE Benjamin <benjamin.faure@inist.fr>
2017-06-28 07:55:30 +0200

    Changes for reCAPTCHA

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,7 @@ class ContactsController < ContactUs::ContactsController
 	def create
 		@contact = ContactUs::Contact.new(params[:contact_us_contact])
 		if (!user_signed_in?)
-			if verify_recaptcha(:message => "You have not added the validation words correctly") && @contact.save
+			if verify_recaptcha(:message => "Please, check the CAPTCHA below.") && @contact.save
 				flash[:notice] = t('contact_us.notices.success')
 				if user_signed_in? then
 			  		redirect_to :controller => 'projects', :action => 'index'
